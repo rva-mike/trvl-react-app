d6c55f17122e19dd64f1dac19ae3112b91b65494
rva-mike <mike.levy.rva@gmail.com>
2023-02-15 21:56:51 -0500

    Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,24 @@ import { Link } from 'react-router-dom';
 
 const STYLES = ['btn--primary', 'btn--outline']
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+const SIZES = ['btn--medium', 'btn--large']
 
+export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+    //if you include a button style, it will be that style, but if you don't include one it will automatically be btn--primary (STYLES[0])
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    )
 
 };
