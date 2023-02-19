671880991b103b025ef5550bde790594d2502915
rva-mike <mike.levy.rva@gmail.com>
2023-02-19 18:37:06 -0500

    HeroSection, Home component, styling, video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { Routes, Route } from 'react-router-dom';
 
@@ -13,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact element={<Home />} />
         </Routes>
       </Router>
 
