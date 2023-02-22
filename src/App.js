7e230bbc2dca291c492b06c67d9fd1088a166296
rva-mike <mike.levy.rva@gmail.com>
2023-02-21 20:54:02 -0500

    route paths to pages in app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Routes, Route } from 'react-router-dom';
-
-
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -15,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
         </Routes>
       </Router>
 
