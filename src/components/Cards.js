0482e46d1760e9d70e4570f32ce104acf8da8325
rva-mike <mike.levy.rva@gmail.com>
2023-02-21 20:01:31 -0500

    Card props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards() {
     return (
@@ -8,7 +9,12 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                        src="images/img-9.jpg"
+                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                        label='Adventure'
+                        path='/services'
+                        />
                     </ul>
                 </div>
             </div>
@@ -16,4 +22,4 @@ function Cards() {
     )
 }
 
-export default Cards
+export default Cards;
